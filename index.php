40e2571d3d6aceb50088e8da34ec73e18b13b18b
Hulett <jameshulett@GR-M-HULETTJ.network.local>
2019-05-13 21:35:06 -0400

    Simple HTML 5 doc

--- index.php
+++ index.php
@@ -8,11 +8,18 @@
             return 'No one you know...';
         }
     }
-
-    echo 'Name from form: ' . handleName();
 ?>
-
-<form method="POST">
-    <input type="text" name="who" />
-    <input type="submit" />
-</form>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Name Form</title>
+    </head>
+    <body>
+        <?php echo 'Name from form: ' . handleName(); ?>
+        <form method="POST">
+            <input type="text" name="who" />
+            <input type="submit" />
+        </form>
+    </body>
+</html>
