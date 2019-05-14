18cd57826eca53d9f321852652956dc353dfa076
Hulett <jameshulett@GR-M-HULETTJ.network.local>
2019-05-13 21:24:51 -0400

    Example function to extract nname

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 
-    $name = $_REQUEST['who'];
-
-    if($name) {
-        echo "Name in URL: $name";
+    function handleName() {
+        $name = $_REQUEST['who'];
+        if ($name) {
+            return $name;
+        } else {
+            return 'No one you know...';
+        }
     }
+
+    echo 'Name from form: ' . handleName();
 ?>
+
 <form method="POST">
     <input type="text" name="who" />
     <input type="submit" />
